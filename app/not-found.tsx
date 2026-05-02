822999123779b917fd1c4052a5a016526c51a8a7
Tomi <aiwithtomx@gmail.com>
2026-05-02 16:51:58 +0200

    Add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="p-4 text-center">
+      <h2 className="text-xl font-bold mb-4">404 - Page Not Found</h2>
+      <a href="/" className="text-blue-600 underline">Go Home</a>
+    </div>
+  );
+}
